b5eb350370701177ab5fb068fafd0c6076efcc08
yangxinming <yangxinming@360.cn>
2024-09-27 20:04:02 +0800

    test monorepo

---
+++ test-monorepo/ vue3-pnpm-monorepo /packages/editor/index.ts
@@ -0,0 +1,26 @@
+export const add = (a, b) => {
+  return a + b;
+};
+
+export const sub = (a, b) => {
+  return a - b;
+};
+export const mul = (a, b) => {
+  return a * b;
+};
+
+export const div = (a, b) => {
+  return a / b;
+};
+
+export const mod = (a, b) => {
+  return a % b;
+};
+
+export const pow = (a, b) => {
+  return a ** b;
+};
+
+export const sqrt = (a) => {
+  return Math.sqrt(a);
+};
---
+++ test-monorepo/1.pnpm-monorepo-demo/packages/ui/index.ts
@@ -0,0 +1,4 @@
+import FormatMoney from './components/FormatMoney.vue';
+import Slider from './components/Slider.vue';
+
+export { FormatMoney, Slider };
