b3043c40fa2f2191cf833646d10155b0d062aab4
yangxinming <yangxinming@360.cn>
2024-09-27 19:54:17 +0800

    test chrome

---
+++ test-chrome/popup.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var clickMeButton = document.getElementById('clickMe');
+  clickMeButton.addEventListener('click', function () {
+    alert('Button clicked!');
+  });
+});
